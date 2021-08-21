330c43fcdcb7c992311c9d5b64009d5a0d9d2ff1
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-21 19:41:41 +0300

    Clean up

--- packages/systems/src/main.ts
+++ packages/systems/src/main.ts
@@ -4,6 +4,5 @@ import path from "path";
 
 const file = readFileSync(path.join(__dirname, "../assets/systems.dat"));
 const systems: any = decode(file);
-console.log(process.env.NODE_ENV);
 
 export const getSystems = () => systems;
