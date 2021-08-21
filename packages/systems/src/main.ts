3692b5eb24ce63a811b2d8282b30ced9a406ae0d
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-21 19:13:20 +0300

    Fix

--- packages/systems/src/main.ts
+++ packages/systems/src/main.ts
@@ -1,7 +1,9 @@
 import { decode } from "@msgpack/msgpack";
 import { readFileSync } from "fs";
+import path from "path";
 
-const file = readFileSync("./assets/systems.dat");
+const file = readFileSync(path.join(__dirname, "../assets/systems.dat"));
 const systems: any = decode(file);
+console.log(process.env.NODE_ENV);
 
 export const getSystems = () => systems;
