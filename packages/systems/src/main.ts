5a88a55a223738e59e8ba2804b20090d9f1b1c0e
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-21 21:20:34 +0300

    Try browser use with json imports

--- packages/systems/src/main.ts
+++ packages/systems/src/main.ts
@@ -1,8 +1,3 @@
-import { decode } from "@msgpack/msgpack";
-import { readFileSync } from "fs";
-import path from "path";
-
-const file = readFileSync(path.join(__dirname, "../assets/systems.dat"));
-const systems: any = decode(file);
+import systems from "../assets/systems.json";
 
 export const getSystems = () => systems;
