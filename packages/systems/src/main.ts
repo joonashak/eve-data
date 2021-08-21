50c31c1278a7879d2fad823f8eb0799cbcd017d6
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-21 21:54:00 +0300

    Add typing to getSystems

--- packages/systems/src/main.ts
+++ packages/systems/src/main.ts
@@ -1,3 +1,13 @@
 import systems from "../assets/systems.json";
 
-export const getSystems = () => systems;
+type System = {
+  name: string;
+  systemEsiId: string;
+  securityStatus: number;
+  securityClass: string;
+  effectId: string | null;
+  regionId: string;
+  whClass: string | null;
+};
+
+export const getSystems = (): System[] => systems;
