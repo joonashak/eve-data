89f00be2c2690d9db63ca71a033f960a1b70b40a
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-08-23 16:37:08 +0300

    Add api to find systems by name or id

---
+++ packages/systems/src/api/findOneSystem.ts
@@ -0,0 +1,30 @@
+import { XOR } from "ts-xor";
+import systems from "../../assets/systems.json";
+import { System } from "./system.type";
+
+/**
+ * Find a system by name or ESI ID. `Error` is thrown upon no match.
+ * @param {object} search - Search object containing the key to search by
+ * (`name` or `id`) and the search string as the corresponding value. Exactly
+ * one search property must be provided. Example: `{ name: "Jita" }`
+ * @returns {System} Star system data.
+ */
+export default (search: XOR<{ name: string }, { id: string }>): System => {
+  const keys = Object.keys(search);
+  if (keys.length !== 1) {
+    throw new Error("Search object must contain exactly one property.");
+  }
+
+  const searchKey = keys[0];
+  if (!["name", "id"].includes(searchKey)) {
+    throw new Error("Invalid search key.");
+  }
+
+  const searchValue = Object.values(search)[0];
+  const match = systems.find((system) => system[searchKey] === searchValue);
+  if (!match) {
+    throw new Error("System not found.");
+  }
+
+  return match;
+};
--- packages/systems/src/main.ts
+++ packages/systems/src/main.ts
@@ -1,4 +1,5 @@
 import systemsData from "../assets/systems.json";
+import findOneSystemApi from "./api/findOneSystem";
 import { System } from "./api/system.type";
 
 const systems: System[] = systemsData;
@@ -7,3 +8,5 @@ const systems: System[] = systemsData;
  * Data for all star systems in the game.
  */
 export default systems;
+
+export const findOneSystem = findOneSystemApi;
