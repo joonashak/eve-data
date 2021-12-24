60eff6f65df7004b5308d3a74d02727c4b6da52b
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-12-24 16:33:10 +0200

    Parse wh effects into nice form

--- updater/src/wormholeEffects.ts
+++ updater/src/wormholeEffects.ts
@@ -3,6 +3,17 @@ import { parse as parseYaml } from "yaml";
 import config from "./config";
 import writeData from "./filesystem/writeData";
 
+const parseEffects = (traits: any) =>
+  traits.miscBonuses.map(({ bonus, bonusText, isPositive }: any) => {
+    const desc = bonusText.en;
+    const description = desc[0].toUpperCase() + desc.slice(1);
+
+    return {
+      strength: isPositive ? bonus : bonus * -1,
+      description,
+    };
+  });
+
 export default async (): Promise<void> => {
   const effectsYaml = await readFile("assets/wormholeEffects.yaml", {
     encoding: "utf8",
@@ -12,7 +23,11 @@ export default async (): Promise<void> => {
 
   const effects = Object.entries(effectsRaw).reduce(
     (acc: any, [key, val]: any) => {
-      acc[key] = val.name.en;
+      acc[key] = {
+        id: key,
+        name: val.name.en.replace(/ Effects$/, ""),
+        effects: parseEffects(val.traits),
+      };
       return acc;
     },
     {}
