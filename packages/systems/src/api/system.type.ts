1b1760eb61bff6d17ffe7023d10c767d6813f9f8
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-12-26 12:43:59 +0200

    Update system type with the new effects

--- packages/systems/src/api/system.type.ts
+++ packages/systems/src/api/system.type.ts
@@ -3,28 +3,78 @@ export type System = {
    * Full system name as it appears in the game.
    */
   name: string;
+
   /**
    * System ESI ID.
    */
   id: number;
+
   /**
    * Exact security status.
    */
   securityStatus: number;
+
   /**
    * Security classification.
    */
   securityClass: "NULL" | "WH" | "HIGH" | "LOW";
+
   /**
-   * Effect id for wormhole systems, `null` for others.
+   * Effect for non-vanilla wormhole systems, `null` for others.
    */
-  effectId: number | null;
+  effect: WormholeEffect | null;
+
   /**
    * Region ESI ID.
    */
   regionId: number;
+
   /**
    * Class (1-6) for wormhole systems, `null` for others.
    */
   whClass: 1 | 2 | 3 | 4 | 5 | 6 | null;
+};
+
+export type WormholeEffect = {
+  /**
+   * Effect ID from SDE.
+   */
+  id: number;
+
+  /**
+   * Effect name including system class.
+   */
+  name: string;
+
+  /**
+   * List of the effect's traits, that is, the actual effects on your ship.
+   */
+  traits: WormholeEffectTrait[];
+};
+
+export type WormholeEffectTrait = {
+  /**
+   * Trait strength percentage, signed.
+   */
+  strength: number;
+
+  /**
+   * Whether this trait is considered positive to the player or not.
+   */
+  isPositive: boolean;
+
+  /**
+   * Trait descriptions.
+   */
+  description: {
+    /**
+     * Description from SDE.
+     */
+    long: string;
+
+    /**
+     * Terse description written by me :)
+     */
+    short: string;
+  };
 };
