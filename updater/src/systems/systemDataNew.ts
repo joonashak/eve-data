8ca5457991aba95c0225f8908e48364f97d1b29c
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-01-27 00:02:28 +0200

    Rewrite system parsing

--- updater/src/systems/systemDataNew.ts
+++ updater/src/systems/systemDataNew.ts
@@ -82,6 +82,43 @@ const getConstellations = async (region: Region): Promise<Constellation[]> => {
   return constellations;
 };
 
+type System = {
+  id: string;
+  name: string;
+  constellation: Constellation;
+};
+
+const getSystems = async (constellation: Constellation): Promise<System[]> => {
+  const systems: System[] = [];
+  const constellationDir = await opendir(constellation.path);
+
+  for await (const constellationEntry of constellationDir) {
+    if (!constellationEntry.isDirectory()) {
+      continue;
+    }
+
+    const systemFilePath = resolve(
+      constellationDir.path,
+      constellationEntry.name,
+      "solarsystem.staticdata"
+    );
+    const systemYaml = await readFile(systemFilePath, {
+      encoding: "utf8",
+    });
+    const systemData = parseYaml(systemYaml);
+    const { solarSystemID } = systemData;
+
+    const system = {
+      id: solarSystemID,
+      name: getNameById(solarSystemID),
+      constellation,
+    };
+    systems.push(system);
+  }
+
+  return systems;
+};
+
 /**
  * Get data for all planetary systems in SDE.
  */
@@ -89,8 +126,15 @@ export default async (): Promise<void> => {
   const kSpaceRegions = await getRegions("sde/fsd/universe/eve", false);
   const anoikisRegions = await getRegions("sde/fsd/universe/wormhole", true);
   const regions = kSpaceRegions.concat(anoikisRegions);
+  const constellations = (
+    await Promise.all(regions.map(getConstellations))
+  ).flat();
+  //console.log(constellations);
   //console.log(regions);
-  console.log(await getConstellations(regions[0]));
+  //console.log(await getConstellations(regions[0]));
+  //console.log(await getSystems(constellations[0]));
+  const systems = (await Promise.all(constellations.map(getSystems))).flat();
+  console.log(systems);
 
   // const systems = await formatSystems(systemsRaw);
   // await writeData(config.dataFiles.systems.name, systems);
