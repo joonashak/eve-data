332f91926896a469dc144db206f697a4e5b4b603
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-01-26 21:08:30 +0200

    Add constellations to system parsing rewrite

--- updater/src/systems/systemDataNew.ts
+++ updater/src/systems/systemDataNew.ts
@@ -9,6 +9,7 @@ import formatSystems from "./formatSystems";
 type Region = {
   id: number;
   name: string;
+  path: string;
   whClass: number | null;
 };
 
@@ -16,7 +17,7 @@ const getRegions = async (
   path: string,
   isAnoikis: boolean
 ): Promise<Region[]> => {
-  const systems: Region[] = [];
+  const regions: Region[] = [];
   const dir = await opendir(path);
 
   for await (const dirent of dir) {
@@ -24,19 +25,61 @@ const getRegions = async (
       continue;
     }
 
-    const regionFilePath = resolve(dir.path, dirent.name, "region.staticdata");
+    const regionPath = resolve(dir.path, dirent.name);
+    const regionFilePath = resolve(regionPath, "region.staticdata");
     const regionYaml = await readFile(regionFilePath, { encoding: "utf8" });
     const regionData = parseYaml(regionYaml);
     const { regionID, wormholeClassID } = regionData;
+
     const region = {
       id: regionID,
       name: getNameById(regionID),
+      path: regionPath,
       whClass: isAnoikis ? wormholeClassID : null,
     };
-    systems.push(region);
+    regions.push(region);
   }
 
-  return systems;
+  return regions;
+};
+
+type Constellation = {
+  id: number;
+  name: string;
+  path: string;
+  region: Region;
+};
+
+const getConstellations = async (region: Region): Promise<Constellation[]> => {
+  const constellations: Constellation[] = [];
+  const regionDir = await opendir(region.path);
+
+  for await (const regionEntry of regionDir) {
+    if (!regionEntry.isDirectory()) {
+      continue;
+    }
+
+    const constellationPath = resolve(regionDir.path, regionEntry.name);
+    const constellationFilePath = resolve(
+      constellationPath,
+      "constellation.staticdata"
+    );
+    const constellationYaml = await readFile(constellationFilePath, {
+      encoding: "utf8",
+    });
+    const constellationData = parseYaml(constellationYaml);
+    const { constellationID } = constellationData;
+
+    const constellation = {
+      id: constellationID,
+      name: getNameById(constellationID),
+      path: constellationPath,
+      region,
+    };
+    constellations.push(constellation);
+  }
+
+  return constellations;
 };
 
 /**
@@ -46,7 +89,8 @@ export default async (): Promise<void> => {
   const kSpaceRegions = await getRegions("sde/fsd/universe/eve", false);
   const anoikisRegions = await getRegions("sde/fsd/universe/wormhole", true);
   const regions = kSpaceRegions.concat(anoikisRegions);
-  console.log(regions);
+  //console.log(regions);
+  console.log(await getConstellations(regions[0]));
 
   // const systems = await formatSystems(systemsRaw);
   // await writeData(config.dataFiles.systems.name, systems);
